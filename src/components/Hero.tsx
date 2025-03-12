f991a6bee9f7359c1a7ee1185e334a34b5be05ce
Kyle Pilon <pilonk@rpi.edu>
2025-03-12 18:30:05 -0400

    Basic structure for Hero.tsx

---
+++ src/components/Hero.tsx
@@ -0,0 +1,12 @@
+import "./../styles/Hero.css";
+
+const Hero = () => {
+  return (
+    <section className="hero">
+      <h1>Kyle Pilon</h1>
+      <p>Software Engineer | Data Enthusiast | Web Developer</p>
+    </section>
+  );
+};
+
+export default Hero;
