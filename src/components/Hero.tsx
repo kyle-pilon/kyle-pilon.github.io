4e2a2999fc7325b96e2b288eca65c8dfdc9c8bdd
Kyle Pilon <pilonk@rpi.edu>
2025-03-13 19:09:32 -0400

    Hero tagline changes

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -8,9 +8,8 @@ const Hero = () => {
             <div className="hero-text">
                 <h1>Hi, I'm Kyle!</h1>
                 <p>
-                I'm a [your profession or passion], passionate about [key interest]. 
-                I enjoy working on [key projects or skills] and strive to create impactful solutions. 
-                Welcome to my portfolio!
+                    I'm a technology professional passionate about building intuitive and efficient software solutions. 
+                    Welcome to my portfolio!
                 </p>
             </div>
             <div className="hero-image">
