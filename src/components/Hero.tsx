ac51acb82c960ac8e82f3b63e6c2362f64cd25bc
Kyle Pilon <pilonk@rpi.edu>
2025-03-13 17:32:00 -0400

    Hero tsx updates

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -1,20 +1,22 @@
 import "./../styles/Hero.css";
+import profilePic from "../assets/professional.jpg";
 
 const Hero = () => {
   return (
     <section className="hero">
-        <div className="hero-content">
+        <div className="hero-container">
             <div className="hero-text">
                 <h1>Hi, I'm Kyle!</h1>
-                <p> I'm a [your profession or passion], passionate about [key interest]. 
-      I enjoy working on [key projects or skills] and strive to create impactful solutions. 
-      Welcome to my portfolio!</p>
+                <p>
+                I'm a [your profession or passion], passionate about [key interest]. 
+                I enjoy working on [key projects or skills] and strive to create impactful solutions. 
+                Welcome to my portfolio!
+                </p>
             </div>
             <div className="hero-image">
-                <img src="/src/assets/professional.jpg" alt="Kyle Pilon" />
+                <img src={profilePic} alt="Kyle Pilon" />
             </div>
         </div>
-
     </section>
   );
 };
