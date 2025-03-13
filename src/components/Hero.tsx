e2500652e19273a31986673e48c2b412fe7a5459
Kyle Pilon <pilonk@rpi.edu>
2025-03-12 22:37:06 -0400

    Image styling updates for the hero

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -3,8 +3,18 @@ import "./../styles/Hero.css";
 const Hero = () => {
   return (
     <section className="hero">
-      <h1>Kyle Pilon</h1>
-      <p>Software Engineer | Data Enthusiast | Web Developer</p>
+        <div className="hero-content">
+            <div className="hero-text">
+                <h1>Hi, I'm Kyle!</h1>
+                <p> I'm a [your profession or passion], passionate about [key interest]. 
+      I enjoy working on [key projects or skills] and strive to create impactful solutions. 
+      Welcome to my portfolio!</p>
+            </div>
+            <div className="hero-image">
+                <img src="/src/assets/professional.jpg" alt="Kyle Pilon" />
+            </div>
+        </div>
+
     </section>
   );
 };
