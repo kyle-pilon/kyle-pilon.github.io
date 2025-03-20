8e783e9b4c3f25ff9e0478bfa3433c97f8cea97d
Kyle Pilon <pilonk@rpi.edu>
2025-03-20 16:42:22 -0400

    Skills section setup

---
+++ src/components/Skills.tsx
@@ -0,0 +1,23 @@
+import "./../styles/Skills.css";
+
+const Skills = () => {
+  return (
+    <section className="skills">
+        <div className="skills-container">
+            <div className="skills-text">
+                <h2>Technical Skills</h2>
+                <p>
+                    I'm proficient in various programming languages, frameworks, and tools, with a strong focus on software engineering and data science.
+                </p>
+            </div>
+
+            <div className="skills-grid">
+                
+            </div>
+        </div>
+
+    </section>
+  );
+};
+
+export default Skills;
